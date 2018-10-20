dabfc0f6b872b50a5023087e7173f6c5dcc6826a
Seu Nome <seuemail@hotmail.com>
2018-10-20 14:38:43 -0300

    primeiro commit

---
+++ Teste-master/projeto/App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace projeto
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/admin-lte/bower_components/bootstrap/dist/css/bootstrap.min.css",
+                      "~/admin-lte/bower_components/font-awesome/css/font-awesome.min.css",
+                      "~/admin-lte/bower_components/Ionicons/css/ionicons.min.css",
+                      "~/admin-lte/css/AdminLTE.min.css",
+                      "~/admin-lte/css/skins/_all-skins.min.css"));
+
+            bundles.Add(new ScriptBundle("~/admin-lte/js").Include(
+                        "~/admin-lte/bower_components/jquery-slimscroll/jquery.slimscroll.min.js",
+                        "~/admin-lte/bower_components/fastclick/lib/fastclick.js",
+                        "~/admin-lte/js/adminlte.min.js",
+                        "~/admin-lte/js/demo.js"));
+        }
+    }
+}
---
+++ Teste-master/projeto/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projeto.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Indicadores()
+        {
+            return View();
+        }
+
+        public ActionResult Iindicadores()
+        {
+            return View();
+        }
+
+        public ActionResult RIindicadores()
+        {
+            return View();
+        }
+    }
+}
